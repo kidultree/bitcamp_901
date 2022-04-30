c328d12c163756b289f6744729d7f190c9c4526c
kidultree <kidultree@naver.com>
2022-05-01 00:48:04 +0900

    commit test SpringMvc Ex3

---
+++ SpringMvcEx3/src/main/java/data/dto/PersonDto.java
@@ -0,0 +1,34 @@
+package data.dto;
+
+public class PersonDto {
+	
+	private String name;
+	private int java;
+	private int spring;
+	private String photo;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getJava() {
+		return java;
+	}
+	public void setJava(int java) {
+		this.java = java;
+	}
+	public int getSpring() {
+		return spring;
+	}
+	public void setSpring(int spring) {
+		this.spring = spring;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+}
---
+++ SpringMvcEx3/src/main/java/data/dto/ShopDto.java
@@ -0,0 +1,36 @@
+package data.dto;
+
+public class ShopDto {
+
+	private String sang;
+	private int su;
+	private int price;
+	private String photo;
+	
+	public String getSang() {
+		return sang;
+	}
+	public void setSang(String sang) {
+		this.sang = sang;
+	}
+	public int getSu() {
+		return su;
+	}
+	public void setSu(int su) {
+		this.su = su;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	
+	
+}
